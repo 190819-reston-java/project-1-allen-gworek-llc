23559977e30e948b4bf6b31b5cfe5b9e93b7b184
Chris Allen <chris.allen070@gmail.com>
2019-09-28 19:54:54 -0400

    Added reimbursement creation functionality

--- project-1/src/main/java/com/revature/servlets/ReimbursementCreationServlet.java
+++ project-1/src/main/java/com/revature/servlets/ReimbursementCreationServlet.java
@@ -1,12 +1,15 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.model.Employee;
 import com.revature.model.Reimbursement;
 import com.revature.repository.DatabaseConnection;
 import com.revature.service.JSONToObject;
@@ -17,10 +20,36 @@ public class ReimbursementCreationServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String JSONFromForm = req.getParameter("reimbursementCreationJSON");
-		int employeeWhoCreatedRequest = Integer.valueOf(req.getParameter("employeeID"));
+
+		int employeeWhoCreatedRequest = JSONToObject.convertEmployeeJSONToObject((String)req.getSession().getAttribute("currentUser")).getID();
 		
-		Reimbursement createdReimbursement = JSONToObject.convertReimbursementJSONtoObject(JSONFromForm);
+		Enumeration<String> passedInParameters = req.getParameterNames();
+
+		Reimbursement createdReimbursement = new Reimbursement();
+		String currentAttribute;
+
+		Field currentField;
+
+		while (passedInParameters.hasMoreElements()) {
+			try {
+				currentAttribute = passedInParameters.nextElement();
+				currentField = createdReimbursement.getClass().getDeclaredField(currentAttribute);
+
+				try {
+					currentField.setAccessible(true);
+					currentField.set(createdReimbursement, req.getParameter(currentAttribute));
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (NoSuchFieldException e) {
+				continue;
+			}
+		}
+		
 		createdReimbursement.setRequestedBy(employeeWhoCreatedRequest);
 		
 		DatabaseConnection dbc = new DatabaseConnection();
@@ -31,6 +60,8 @@ public class ReimbursementCreationServlet extends HttpServlet {
 			queryToInsertReimbursement = QueryProcessor.specifyTable(queryToInsertReimbursement, "reimbursements");
 			
 			dbc.executeQueryInDatabase(queryToInsertReimbursement);
+			
+			resp.sendRedirect("/project-1/submit-reimbursement-image.html");
 		} catch (IllegalArgumentException | IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
