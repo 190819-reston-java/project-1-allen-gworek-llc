00a76078c031a74589688a28c791640e878c3a33
Fothu <moosecalibur@yahoo.com>
2019-09-30 20:43:36 -0400

    added reimbursement approval functionality

---
+++ project-1/src/main/webapp/managerpending.js
@@ -0,0 +1,84 @@
+'use strict';
+
+fetch("http://localhost:8080/project-1/reimbursements/pending/all")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((thisJson) => {
+        let Json = thisJson;
+        var i;
+        for (i = 0; i < Json.length; i++) {
+            var tablerow = document.createElement("tr");
+            var tabledataID = document.createElement("td");
+            tabledataID.innerText = Json[i].id;
+            var tabledataAmount = document.createElement("td");
+            tabledataAmount.innerText = Json[i].dollarAmount;
+            var tabledataRec = document.createElement("td");
+            var tabledataResolved = document.createElement("td");
+
+            var recLink = document.createElement("a");
+            recLink.setAttribute("class", "nav-link");
+            var imageURLText;
+            recLink.setAttribute("href", Json[i].imgURL);
+            if (Json[i].imgUrl !== undefined) {
+                recLink.innerText = "Receipt";
+            } else {
+                //MAKE THIS WORK
+                recLink.disabled = true;
+            }
+            tabledataRec.appendChild(recLink);
+
+            if (Json[i].approved = true) {
+                tabledataResolved.innerText = "Approved";
+            } else {
+                tabledataResolved.innerText = "Denied";
+            }
+
+
+            var tabledataDecision = document.createElement("td");
+
+            var tabledataDecisionForm = document.createElement("form");
+            tabledataDecisionForm.setAttribute("class", "form-inline");
+            tabledataDecisionForm.setAttribute("action", "/project-1/reimbursements/resolve");
+            tabledataDecisionForm.setAttribute("method", "POST");
+
+            var tabledataDecisionSelect = document.createElement("select");
+            tabledataDecisionSelect.setAttribute("name", "updatereimbursement");
+            tabledataDecisionSelect.setAttribute("class", "custom-select my-1 mr-sm-2");
+            tabledataDecisionSelect.setAttribute("id", "inlineFormCustomSelectPref");
+
+            var tabledataDecisionValueTrue = document.createElement("option");
+            tabledataDecisionValueTrue.setAttribute("value", `${Json[i].id}+True`);
+            tabledataDecisionValueTrue.innerText = "Approve";
+
+            var tabledataDecisionValueFalse = document.createElement("option");
+            tabledataDecisionValueFalse.setAttribute("value", `${Json[i].id}+False`);
+            tabledataDecisionValueFalse.innerText = "Deny";
+
+            var tabledataDecisionBtn = document.createElement("button");
+            tabledataDecisionBtn.setAttribute("type", "submit");
+            tabledataDecisionBtn.setAttribute("class", "btn btn-dark");
+            tabledataDecisionBtn.innerText = "Submit";
+
+            tabledataDecisionSelect.appendChild(tabledataDecisionValueTrue);
+            tabledataDecisionSelect.appendChild(tabledataDecisionValueFalse);
+            tabledataDecisionForm.appendChild(tabledataDecisionSelect);
+            tabledataDecisionForm.appendChild(tabledataDecisionBtn);
+            tabledataDecision.appendChild(tabledataDecisionForm);
+
+
+
+
+
+
+            tablerow.appendChild(tabledataID);
+            tablerow.appendChild(tabledataAmount);
+            tablerow.appendChild(tabledataDecision);
+            tablerow.appendChild(tabledataRec);
+
+            document.querySelector(".tablebody").appendChild(tablerow);
+
+        }
+    })
+    .catch(console.log);
