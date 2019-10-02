5db19936156a351e454d47e2276837fffad0c71e
JGworek <jonathangworek@gmail.com>
2019-10-02 10:16:43 -0400

    bug fixes 3

--- project-1/src/main/webapp/login/takenemail.js
+++ project-1/src/main/webapp/login/takenemail.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var previousPage = document.referrer;
+
 window.onload = checkEmail();
 
 function checkEmail() {
-    if (document.referrer === "signuppage.html") {
+    if (previousPage === "http://localhost:8080/project-1/login/signuppage.html") {
         var oopsemail = document.createElement("h5");
         oopsemail.setAttribute("color", "red");
         oopsemail.innerText = "*That email is already on file"
