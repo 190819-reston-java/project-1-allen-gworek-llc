c5d5c0a274be326caa6804cb61989b6debe0151d
JGworek <jonathangworek@gmail.com>
2019-10-02 10:07:13 -0400

    bug fixing 2

--- project-1/src/main/webapp/login/takenemail.js
+++ project-1/src/main/webapp/login/takenemail.js
@@ -6,7 +6,7 @@ function checkEmail() {
     if (document.referrer === "signuppage.html") {
         var oopsemail = document.createElement("h5");
         oopsemail.setAttribute("color", "red");
-        oopsemail.innerText = "That email is already on file"
+        oopsemail.innerText = "*That email is already on file"
         
         document.querySelector(".emailoops").appendChild(oopsemail);
     }
